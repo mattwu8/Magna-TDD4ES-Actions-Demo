1645dbb73eaf8153608f0325c6b96391a35642ae
EZ <88570645+EZ337@users.noreply.github.com>
2024-10-08 21:49:32 -0400

    Dummy left behind the makefile

---
+++ TDD4ES/tests/tmpMakefile.mk
@@ -0,0 +1,40 @@
+# If you installed CppUTest from apt, leave CPPUTEST_HOME as is, otherwise, change it to the path where you installed CppUTest
+CPPUTEST_HOME = ../../
+CPPFLAGS += -I$(CPPUTEST_HOME)/include
+
+# CXXFLAGS += -include $(CPPUTEST_HOME)/include/CppUTest/MemoryLeakDetectorNewMacros.h
+# CFLAGS += -include $(CPPUTEST_HOME)/include/CppUTest/MemoryLeakDetectorMallocMacros.h
+
+LD_LIBRARIES = -L$(CPPUTEST_HOME)/lib -lCppUTest -lCppUTestExt
+
+# Flags
+CPPFLAGS += -I../test_mocks	# Add flag to include mocking headers
+CPPFLAGS += -I..			# Add flag to include parent directory
+# CPPFLAGS += -I../../../
+CPPUFLAGS += -DUNIT_TEST	# Test flag (to trigger certain preprocessor directives)
+CPPFLAGS += --coverage -O0	# Add flags for code coverage
+
+# Compiler. Use g++ which can compile both C and C++ files
+CXX = g++
+
+# Main test file
+SRCS = all_tests.cpp
+
+# Add tests
+SRCS += matthew_test.cpp
+
+# Output executable
+TARGET = all_tests.testcase
+
+# Build target
+$(TARGET): $(SRCS)
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $(TARGET) $(SRCS) $(LD_LIBRARIES)
+
+# Clean target
+clean:
+	rm -f $(TARGET)
+	rm -f *.gcda
+	rm -f *.gcno
+	rm -f *.info
+	rm -r html/
+	rm -r xml/
